266de00591ea53131e95890898d18133ee3f6d65
Wendell Alves <wendellmccaulay@hotmail.com>
2023-09-30 00:43:11 -0300

    Remover anotação por enquanto

--- src/main/java/br/com/tech/challenge/TechChallengeApplication.java
+++ src/main/java/br/com/tech/challenge/TechChallengeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
-@EnableRedisRepositories(basePackages = {"br.com.tech.challenge.redis"})
 public class TechChallengeApplication {
 
     public static void main(String[] args) {
