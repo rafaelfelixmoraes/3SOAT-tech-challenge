52431db3439a46f520cccf31d53f60741b67b951
Wendell Alves <wendellmccaulay@hotmail.com>
2023-10-01 21:45:54 -0300

    Criar classe de controller

--- src/main/java/br/com/tech/challenge/api/ClienteController.java
+++ src/main/java/br/com/tech/challenge/api/ClienteController.java
@@ -1,13 +1,12 @@
 package br.com.tech.challenge.api;
 
-import br.com.tech.challenge.domain.dto.ProdutoDTO;
-import br.com.tech.challenge.servicos.ProdutoService;
+import br.com.tech.challenge.domain.dto.ClienteDTO;
+import br.com.tech.challenge.servicos.ClienteService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,23 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/produtos")
-public class ProdutoController {
+@RequestMapping("/clientes")
+public class ClienteController {
 
-    private final ProdutoService produtoService;
+    private final ClienteService clienteService;
 
     private final ModelMapper mapper;
 
-    @Operation(description = "Endpoint para criar um Produto")
+    @Operation(description = "Endpoint para criar um Cliente")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Produto criado com sucesso."),
-            @ApiResponse(responseCode = "400", description = "Produto inválido."),
+            @ApiResponse(responseCode = "200", description = "Cliente criado com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Cliente inválido."),
             @ApiResponse(responseCode = "401", description = "Não autorizado.")
         }
     )
     @PostMapping
-    public ResponseEntity<ProdutoDTO> salvar(@RequestBody ProdutoDTO produtoDTO) {
-        return ResponseEntity.ok().body(mapper.map(produtoService.salvar(produtoDTO), ProdutoDTO.class));
+    public ResponseEntity<ClienteDTO> salvar(@RequestBody ClienteDTO clienteDTO) {
+        return ResponseEntity.ok().body(mapper.map(clienteService.salvar(clienteDTO), ClienteDTO.class));
     }
 
 }
