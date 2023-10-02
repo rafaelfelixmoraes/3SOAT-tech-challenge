5250a519bfc2542465ffbb55f3f14738c273bb18
Wendell Alves <wendellmccaulay@hotmail.com>
2023-10-01 21:23:43 -0300

    Adicionar DTO de clientes

---
+++ src/main/java/br/com/tech/challenge/domain/dto/ClienteDTO.java
@@ -0,0 +1,13 @@
+package br.com.tech.challenge.domain.dto;
+
+public class ClienteDTO {
+
+    private Long id;
+
+    private String nome;
+
+    private String cpf;
+
+    private String email;
+
+}
