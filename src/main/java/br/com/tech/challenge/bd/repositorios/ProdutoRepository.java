b5e204cb545db1aeaac134995c97cc4a5f033662
hugompereira2 <55194253+hugompereira2@users.noreply.github.com>
2023-10-17 16:54:54 -0300

    listar e remover produtos

--- src/main/java/br/com/tech/challenge/bd/repositorios/ProdutoRepository.java
+++ src/main/java/br/com/tech/challenge/bd/repositorios/ProdutoRepository.java
@@ -2,8 +2,11 @@ package br.com.tech.challenge.bd.repositorios;
 
 import br.com.tech.challenge.domain.entidades.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    Page<Produto> findByDescricaoContainingIgnoreCase (String descricao, Pageable pageable);
 }
