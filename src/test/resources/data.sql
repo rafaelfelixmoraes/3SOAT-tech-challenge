035a2878da3189b946eb87625b6e65985e1ddde1
Wendell Alves <wendellmccaulay@hotmail.com>
2023-10-29 01:30:30 -0300

    Teste de integração

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -6,3 +6,10 @@ VALUES (5.00, 2, 10, 'Coca Cola');
 
 INSERT INTO produto (valor_unitario, categoria_id, id, descricao)
 VALUES (5.00, 2, 15, 'Pepsi');
+
+INSERT INTO public.pedido(
+    senha_retirada, valor_total, cliente_id, id, status_pedido, data_hora)
+VALUES (696328, 5.00, 10, 10, 'RECEBIDO', '2023-10-26 22:10:07.949088');
+
+INSERT INTO public.pagamento (id, data_hora_pagamento, valor_total, pedido_id, qr_data, status_pagamento)
+VALUES (10, '2023-10-26 22:10:12.640283', 5.00, 10, NULL, 'AGUARDANDO_PAGAMENTO');
