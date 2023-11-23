155de19b7527974dba461494f5ab11552058b408
Wendell Alves <wendellmccaulay@hotmail.com>
2023-11-22 22:11:15 -0300

    Adicionando log para classe ProdutoService

--- src/main/java/br/com/tech/challenge/servicos/ProdutoService.java
+++ src/main/java/br/com/tech/challenge/servicos/ProdutoService.java
@@ -6,6 +6,7 @@ import br.com.tech.challenge.domain.dto.ProdutoDTO;
 import br.com.tech.challenge.domain.dto.ProdutoUpdateDTO;
 import br.com.tech.challenge.domain.entidades.Produto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProdutoService {
 
     private final ProdutoRepository produtoRepository;
@@ -28,11 +30,13 @@ public class ProdutoService {
 
     @Transactional
     public Produto save(ProdutoDTO produtoDTO) {
+        log.info("Salvando produto {}", produtoDTO);
         return produtoRepository.save(mapper.map(produtoDTO, Produto.class));
     }
 
     @Transactional
     public Produto update(final ProdutoUpdateDTO produtoUpdateDTO) {
+        log.info("Atualizando produto {}", produtoUpdateDTO);
         var produto = produtoRepository.findById(produtoUpdateDTO.getId())
                 .orElseThrow(() -> new ObjectNotFoundException("Nenhum registro encontrado para o id informado"));
 
@@ -42,12 +46,15 @@ public class ProdutoService {
     }
 
     public Page<Produto> list(Long id, Long categoria, int pagina, int tamanho) {
+        log.info("Listando os produtos com os seguintes parametros: id={id}, categoria={}, pagina={}, tamanho={}",
+                id, categoria, pagina, tamanho);
         Pageable pageable = PageRequest.of(pagina, tamanho, Sort.by("descricao"));
 
         if (ObjectUtils.anyNotNull(id, categoria)) {
             return produtoRepository.findByIdOrCategoriaId(id, categoria, pageable);
         }
 
+        log.info("Retornando lista de produtos");
         return produtoRepository.findAll(pageable);
     }
     @Transactional
@@ -56,14 +63,17 @@ public class ProdutoService {
             throw new ObjectNotFoundException("Nenhum registro encontrado para o ID informado");
         }
 
+        log.info("Deletando o produto de id {}", id);
         produtoRepository.deleteById(id);
     }
 
     public long count(Long id, List<Produto> produtos) {
+        log.info("Contando os produtos com o id {}", id);
         return produtos.stream().filter(produto -> produto.getId().equals(id)).count();
     }
 
     public Optional<Produto> findById(Long id) {
+        log.info("Buscando por id {}", id);
         return produtoRepository.findById(id);
     }
 
